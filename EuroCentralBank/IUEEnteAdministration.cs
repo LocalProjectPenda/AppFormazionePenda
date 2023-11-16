b361366c315514cb2532458308bb67024b07b613
smartshopperpay <smartshopperpay@gmail.com>
2023-11-16 11:31:43 +0100

    #Correzione

--- EuroCentralBank/IUEEnteAdministration.cs
+++ EuroCentralBank/IUEEnteAdministration.cs
@@ -4,16 +4,15 @@
     {
         public string BorderReDefinition(Parlement parlement);  // I medoti dell'interfaccia sono sempre PUBLIC 
         public void WelfareService();
-        //  ->  Hai gia quest metodi da IEnteAdminstrative. non devi recrearli di nuovoa livello di Interface
-        // -> L'overloading va fatto solo a livello di classe 
 
-        /*
-         
-        public void HNS() { }
-        public void LAWSystem() { }
-        public void EducationSystem() { } 
 
-         */
+        ///   Hai gia quest metodi da IEnteAdminstrative. non devi recrearli di nuovoa livello di Interface
+        ///   L'overloading va fatto solo a livello di classe    
+        //public void HNS() { }
+        //public void LAWSystem() { }
+        //public void EducationSystem() { }
+
+
 
     }
 }
--- EuroCentralBank/Provincia.cs
+++ EuroCentralBank/Provincia.cs
@@ -7,16 +7,18 @@
     {
         public Provincia(string nameGeo, decimal area) : base(nameGeo, area) { }
 
-        //  NON DEVI FARE OVVERRIDE dei metodi dell Interface. L'OVERRRIDE va fatto solo per i metodi abstract e virtual 
 
-        /*
-        public override void WelfareService() { }
-        public abstract void HNS(UECitizenPublicService ID) { }
-        public abstract void EducationSystem(UECitizenPublicService ID) { }
+        ///  NON DEVI FARE OVVERRIDE dei metodi dell'Interface. L'OVERRRIDE va fatto solo per i metodi abstract e virtual 
+        /// Per fare ORERRIDE dei metodi dell'Interface basta aggiungere il  corpo della funzione 
 
-        */
 
-        // Per implentare i metodi dell'interfaccia  basta portarli dentro la classe aggiungere il corpo della funzione. 
+        //public override void WelfareService() { }
+        //public abstract void HNS(UECitizenPublicService ID) { }
+        //public abstract void EducationSystem(UECitizenPublicService ID) { }
+
+
+
+        /// Per implentare i metodi dell'interfaccia  basta portarli dentro la classe aggiungere il corpo della funzione. 
         public string BorderReDefinition(Parlement parlement)
         {
             return string.Empty;
